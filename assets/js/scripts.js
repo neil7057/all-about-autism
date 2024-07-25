2ea9ac1f879327545d7465d777a24bc8d623c1a9
Neil Allen <neil7057@gmail.com>
2024-07-25 18:54:06 +0000

    fix error in background file path

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,16 +16,16 @@ $(document).ready(function(){
 
   // create array of images for card fronts on myths page
 const mythImages = [
-  'url("../..assets/images/disease.jpg")',  
-  'url("/assets/images/cure-modified.png")',
-  'url("../../assets/images/vaccine.jpg")',
-  'url("../../assets/images/learn-disab.jpg")',
-  'url("../../assets/images/savant.jpg")',
-  'url("../../assets/images/emotion-mod.png")',
-  'url("../../assets/images/graph.jpg")',
-  'url("../../assets/images/days.jpg")',
-  'url("../../assets/images/non-verbal.png")',
-  'url("../../assets/images/badparents.jpg")'
+  'url("../assets/images/disease.jpg")',  
+  'url("../assets/images/cure-modified.png")',
+  'url("../assets/images/vaccine.jpg")',
+  'url("../assets/images/learn-disab.jpg")',
+  'url("../assets/images/savant.jpg")',
+  'url("../assets/images/emotion-mod.png")',
+  'url("../assets/images/graph.jpg")',
+  'url("../assets/images/days.jpg")',
+  'url("../assets/images/non-verbal.png")',
+  'url("../assets/images/badparents.jpg")'
 ];
 
 // populate card images on myths page only on screen sizes above 360px.
@@ -33,5 +33,6 @@ if (document.documentElement.clientWidth > 326) {
   var backImage = document.getElementsByClassName("myth-card-front"); 
   for (var i = 0; i < backImage.length; i++) {
   backImage[i].style.backgroundImage = mythImages[i];
+  console.log(mythImages[i]);
   }
 };
